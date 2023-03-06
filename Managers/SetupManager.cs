e8c9993cd69c1d8adcb3649afe3bf4da87824fe4
Millzy <millzyyt08@gmail.com>
2023-03-06 13:31:21 +0000

    check for internet connection

---
+++ Managers/SetupManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Radio.Managers
+{
+    public class SetupManager : IInitializable, IDisposable
+    {
+        [Inject]
+        private StationLoader stationLoader;
+
+        public void Initialize()
+        {
+            if (!NetUtils.InternetGetConnected())
+            {
+                stationLoader.AbleToLoad = false;
+                stationLoader.Errors.Add("No internet connection");
+            }
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
