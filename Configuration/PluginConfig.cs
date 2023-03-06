d26c26d1e81597f7b764fda619f9fd6b72590fbf
Millzy <millzyyt08@gmail.com>
2023-03-06 22:40:41 +0000

    re-arrange and add some new stuff

--- Configuration/PluginConfig.cs
+++ Configuration/PluginConfig.cs
@@ -7,11 +7,14 @@ namespace Radio.Configuration
 {
     public class PluginConfig
     {
-        public virtual string SelectedList { get; set; } = null;
-        public virtual string SelectedStation { get; set; } = null;
-        public virtual bool Enabled { get; set; } = false;
-        public virtual float Volume { get; set; }
-        public bool Sound3D { get; set; }
-        //public Vector3 Position { get; set; }
+        public virtual bool Enabled { get; set; } = true;
+        public virtual bool Sound3D { get; set; } = true;
+        public virtual bool DisableMenuMusic { get; set; } = true;
+        public virtual float Volume { get; set; } = 1f;
+        public virtual float Rotation { get; set; } = 180f;
+        public virtual string DefaultStation { get; set; } = "TruckersFM";
+        public virtual string SelectedList { get; set; } = string.Empty;
+        public virtual string SelectedStation { get; set; } = string.Empty;
+        public virtual Vector2 Position { get; set; } = new Vector2(0f, -2f);
     }
 }
