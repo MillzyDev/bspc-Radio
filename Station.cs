e873af10d9e129151516306b016c68efe84e688a
Millzy <millzyyt08@gmail.com>
2023-03-06 12:05:04 +0000

    radio station storage

---
+++ Station.cs
@@ -0,0 +1,14 @@
+﻿namespace Radio
+{
+    public class Station
+    {
+        public Station(string name, string url)
+        {
+            Name = name;
+            URL = url;
+        }
+
+        public string Name { get; private set; }
+        public string URL { get; private set; }
+    }
+}
