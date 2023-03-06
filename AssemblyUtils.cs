aa54c6e24bea9e5fb4b7e8a050cf20995d3d3d32
Millzy <millzyyt08@gmail.com>
2023-03-06 13:30:15 +0000

    loading resources

---
+++ AssemblyUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Radio
+{
+    public static class AssemblyUtils
+    {
+        public static Stream ReadResource(string path)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return assembly.GetManifestResourceStream(path);
+        }
+
+        public static string ReadResourceAsString(string path)
+        {
+            using (var stream = ReadResource(path))
+            {
+                if (stream == null)
+                    return null;
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
+
+// penis
