9afa7d775df1de43060f935b63b26c6dc2066619
Millzy <millzyyt08@gmail.com>
2023-03-06 13:30:31 +0000

    merging dictionaries

---
+++ DictionaryUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Radio
+{
+    public static class DictionaryUtils
+    {
+        public static Dictionary<K, V> Merge<K, V>(params Dictionary<K, V>[] dictionaries)
+        {
+            return dictionaries.SelectMany(x => x)
+                            .ToDictionary(x => x.Key, y => y.Value);
+        }
+    }
+}
