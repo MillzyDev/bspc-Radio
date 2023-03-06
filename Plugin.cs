cad32f2c6117f03cda26d27f1e4e4cd20c8b883f
Millzy <millzyyt08@gmail.com>
2023-03-06 12:05:46 +0000

    init logic

--- Plugin.cs
+++ Plugin.cs
@@ -8,53 +8,31 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
+using Conf = IPA.Config.Config;
+using SiraUtil.Zenject;
+using IPA.Loader;
+using Radio.Configuration;
+using Radio.Installers;
 
 namespace Radio
 {
-    [Plugin(RuntimeOptions.SingleStartInit)]
+    [Plugin(RuntimeOptions.SingleStartInit), NoEnableDisable]
     public class Plugin
     {
         internal static Plugin Instance { get; private set; }
         internal static IPALogger Log { get; private set; }
 
         [Init]
-        /// <summary>
-        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
-        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
-        /// Only use [Init] with one Constructor.
-        /// </summary>
-        public void Init(IPALogger logger)
+        public void Init(Conf conf, IPALogger logger, Zenjector zenjector)
         {
-            Instance = this;
-            Log = logger;
-            Log.Info("Radio initialized.");
-        }
-
-        #region BSIPA Config
-        //Uncomment to use BSIPA's config
-        /*
-        [Init]
-        public void InitWithConfig(Config conf)
-        {
-            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
-            Log.Debug("Config loaded");
-        }
-        */
-        #endregion
-
-        [OnStart]
-        public void OnApplicationStart()
-        {
-            Log.Debug("OnApplicationStart");
-            new GameObject("RadioController").AddComponent<RadioController>();
-
-        }
-
-        [OnExit]
-        public void OnApplicationQuit()
-        {
-            Log.Debug("OnApplicationQuit");
-
+            PluginConfig config = conf.Generated<PluginConfig>();
+            zenjector.UseLogger(logger);
+            zenjector.Install(Location.App, Container =>
+            {
+                Container.Bind<PluginConfig>().FromInstance(config).AsSingle();
+                Container.Bind<Plugin>().FromInstance(this).AsSingle();
+            });
+            zenjector.Install<MenuInstaller>(Location.Menu);
         }
     }
 }
