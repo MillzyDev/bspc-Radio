762994d32b9abfb94edc678c75b1ac27d1642e58
Millzy <millzyyt08@gmail.com>
2023-03-06 12:13:29 +0000

    internet checks

---
+++ NetUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    public static class NetUtils
+    {
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+        public static bool InternetGetConnected()
+        {
+            return InternetGetConnectedState(out _, 0);
+        }
+    }
+}
