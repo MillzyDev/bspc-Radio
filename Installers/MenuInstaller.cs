a418e74b4de09c6e00f890922e0124c2bb31e880
Millzy <millzyyt08@gmail.com>
2023-03-06 12:05:55 +0000

    base menu installer

---
+++ Installers/MenuInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Radio.Installers
+{
+    public class MenuInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
