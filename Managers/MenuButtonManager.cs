6d0c3e175d82fdfb3a98e3efbfcfd67b721a9e0f
Millzy <millzyyt08@gmail.com>
2023-03-07 09:46:09 +0000

    basic menu button

--- Managers/MenuButtonManager.cs
+++ Managers/MenuButtonManager.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.MenuButtons;
+using Radio.FlowCoordinators;
+using System;
+using Zenject;
 
 namespace Radio.Managers
 {
-    public class UIManager
+    public class MenuButtonManager : IInitializable, IDisposable
     {
-        
+        [Inject]
+        private MainFlowCoordinator _mainFlowCoordinator;
+        [Inject]
+        private RadioSettingsFlowCoordinator _radioSettingsFlowCoordinator;
+
+        private readonly MenuButton _menuButton;
+
+        MenuButtonManager() 
+        {
+            _menuButton = new MenuButton(nameof(Radio), ShowFlowCoordinator);
+        }
+
+        public void Initialize()
+        {
+            MenuButtons.instance.RegisterButton(_menuButton);
+        }
+
+        private void ShowFlowCoordinator()
+        {
+            _mainFlowCoordinator.PresentFlowCoordinator(_radioSettingsFlowCoordinator, animationDirection: HMUI.ViewController.AnimationDirection.Vertical);
+        }
+
+        public void Dispose()
+        {
+            if (BSMLParser.IsSingletonAvailable && MenuButtons.IsSingletonAvailable)
+                MenuButtons.instance.UnregisterButton(_menuButton);
+        }
     }
 }
