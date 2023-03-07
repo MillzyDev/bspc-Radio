a9d10e2e76532c19f35e608a5925053188991bea
Millzy <millzyyt08@gmail.com>
2023-03-07 09:46:01 +0000

    basic flow

--- FlowCoordinators/RadioSettingsFlowCoordinator.cs
+++ FlowCoordinators/RadioSettingsFlowCoordinator.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using HMUI;
+using Radio.Configuration;
+using SiraUtil.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace Radio.FlowCoordinators
 {
-    internal class RadioSettingsFlowCoordinator
+    public class RadioSettingsFlowCoordinator : FlowCoordinator
     {
+        [Inject]
+        private PluginConfig _config;
+        [Inject]
+        private SiraLog _logger;
+        [Inject]
+        private MainFlowCoordinator _mainFlowCoordinator;
+
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            if (firstActivation)
+            {
+                SetTitle(nameof(Radio));
+                showBackButton = true;
+
+                //ProvideInitialViewControllers()
+            }
+        }
     }
 }
